c06525b33d4304bdddfb9f59e39e7f98c816d1e6
Gourav Yadav <v0idgy@gmail.com>
2025-06-27 15:39:29 +0530

    Test Commit Passed

--- python-test.py
+++ python-test.py
@@ -1,12 +1,7 @@
-import requests
+# import requests
 
-url = "http://localhost:8000/heroes/"
-data = {
-    "id": 1,
-    "name": "my hero",
-    "secret_name": "austing",
-    "age": 12
-}
+# url = "http://localhost:8000/heroes/"
+# data = {"id": 1, "name": "my hero", "secret_name": "austing", "age": 12}
 
-response = requests.post(url, json=data)
-print(response.json())
+# response = requests.post(url, json=data)
+# print(response.json())
